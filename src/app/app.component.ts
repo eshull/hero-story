3c68438a0218fc3e50c18bde19f1d3b72aef9f80
Eric Shull and Thad Donaghue <thad@>
2018-08-13 14:38:46 -0700

    add player info through app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour Of Heroes';
+  study: string = 'Wizard';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  firstPlayer = {
+    study: 'Level five sorcerer'
+  }
 }
